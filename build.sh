93ac72badc37ee114c0bf53022f28407c08b7fd6
Nathan Daly <nhdaly@gmail.com>
2018-03-19 12:17:05 -0500

    specify bundle_identifier

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ jl_main="pongmain"
 certificate="Nathan Daly"
 
 julia ~/src/build-jl-app-bundle/build_app.jl -v \
- -R assets -L "libs/*" --icns icns.icns \
+ -R assets -L "libs/*" --bundle_identifier "com.nhdaly.powerpong" --icns icns.icns \
  --certificate "$certificate" --entitlements "./entitlements.entitlements" \
  --app_version=0.2 "$jl_main.jl" "$APP_NAME"
