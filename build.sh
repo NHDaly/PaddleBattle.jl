ee76c90792d526e4085fde52a7003143e211b223
Nathan Daly <nhdaly@gmail.com>
2018-03-19 11:48:23 -0500

    Moved codesigning to build_app.jl package

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 APP_NAME="PowerPong"
 jl_main="pongmain"
 certificate="Nathan Daly"
 
-julia ~/src/build-jl-app-bundle/build_app.jl -v -R assets -L "libs/*" --icns icns.icns --app_version=0.2 "$jl_main.jl" "$APP_NAME"
-
-./sign-application.sh "builddir/$APP_NAME.app" "$certificate"
-
-codesign --entitlements ./entitlements.entitlements -fs "$certificate" "builddir/$APP_NAME.app/Contents/MacOS/$jl_main"
+julia ~/src/build-jl-app-bundle/build_app.jl -v \
+ -R assets -L "libs/*" --icns icns.icns \
+ --certificate "$certificate" --entitlements "./entitlements.entitlements" \
+ --app_version=0.2 "$jl_main.jl" "$APP_NAME"
