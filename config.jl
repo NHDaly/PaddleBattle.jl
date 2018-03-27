fd81cf9f29700a9612b9493ba4d1ddf7ebaa966f
Nathan Daly <nhdaly@gmail.com>
2018-03-27 13:58:21 -0500

    Added config parsing function to allow dynamically modifying config values

---
+++ config.jl
@@ -0,0 +1,33 @@
+config_read_timer = Timer()
+config_print_error_timer = Timer()
+
+config_reload_time_s = 0.5  # seconds
+
+"""
+    reloadConfigsFile()
+
+ Reload "configs.json" and assign global variables for top-level keys.
+ This should be called once for release build during compile time, and can be
+ called as part of the run-loop for debug builds to allow on-the-fly editing of
+ the configs file.
+ """
+ function reloadConfigsFile()
+     if elapsed(config_read_timer) > config_reload_time_s
+         try
+             include("configs.jl")
+         catch e
+             print_config_error("ERROR: Failed to reload Configs File!:\n '$(e)'")
+         end
+         start!(config_read_timer)
+     end
+end
+
+function print_config_error(msg)
+    if elapsed(config_print_error_timer) > 5
+        println(msg)
+        start!(config_print_error_timer)
+    end
+end
+
+# Run once during compile time.
+reloadConfigsFile()
---
+++ configs.jl
@@ -0,0 +1,7 @@
+defaultFontName = "/Users/daly/Documents/developer/fonts/press-start-2p/PressStart2P-Regular.ttf"
+defaultFontSize = 16
+
+kMenuButtonFontSize = 12
+kKeyButtonFontSize = 10
+kMenuButtonColor = SDL2.Color(80, 80, 200, 255)
+kKeySettingButtonColor = SDL2.Color(185, 55, 48, 255)
