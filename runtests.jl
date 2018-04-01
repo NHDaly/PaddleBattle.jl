86131a38b2a85541a7b820cb525453a4cb9889b3
Nathan Daly <nhdaly@gmail.com>
2018-04-01 01:06:30 -0400

    fixed object tests

--- runtests.jl
+++ runtests.jl
@@ -1,33 +1,43 @@
 using Base.Test
+using SDL2
 include("objects.jl")
+include("configs.jl")
 
 # Objects tests
-l = Line(WorldPos(-10,1), WorldPos(-10,-1))
-p = Paddle(WorldPos(0,0), 20)
+kZeroVel = Vector2D(0,0)
+
+@testset "isColliding(paddle, line)" begin
+l = LineSegment(WorldPos(-10,1), WorldPos(-10,-1))
+p = Paddle(WorldPos(0,0), kZeroVel, 20)
 @test isColliding(p, l, 0)
 @test isColliding(p, l, 10)
+end
 
+@testset "willCollide(ball, paddle, dt)" begin
+@testset "dt" begin
 b = Ball(WorldPos(-10,-1), Vector2D(0,2))
-p = Paddle(WorldPos(0,0), 30)
+p = Paddle(WorldPos(0,0), kZeroVel, 30)
 @test !willCollide(b, p, 0)
 @test willCollide(b, p, 1)
 @test willCollide(b, p, .5)
 @test !willCollide(b, p, .4)
-p = Paddle(WorldPos(0,0), 5)
+p = Paddle(WorldPos(0,0), kZeroVel, 5)  # too short
 @test !willCollide(b, p, 1)
 
 b = Ball(WorldPos(0,0), Vector2D(0,-2))
-p = Paddle(WorldPos(0,-10), 10)
+p = Paddle(WorldPos(0,-100), kZeroVel, 10)
 @test !willCollide(b, p, 1)
+end
 
+@testset "collision detection" begin
 b = Ball(WorldPos(-10,9), Vector2D(0,2))
-p = Paddle(WorldPos(-8,10), 5)
+p = Paddle(WorldPos(-8,10), kZeroVel, 5)
 @test willCollide(b, p, 1)
 b = Ball(WorldPos(-10,11), Vector2D(0,2))
 @test !willCollide(b, p, 1)
 
 b = Ball(WorldPos(0,-195), Vector2D(0,-200))
-p = Paddle(WorldPos(0,-200), 5)
+p = Paddle(WorldPos(0,-200), kZeroVel, 5)
 @test willCollide(b, p, 1)
 b = Ball(WorldPos(0,-200), Vector2D(0,-200000))
 @test willCollide(b, p, 1)
@@ -36,3 +46,6 @@ b = Ball(WorldPos(0,0), Vector2D(0,-200))
 @test willCollide(b, p, 1)
 b = Ball(WorldPos(0,0), Vector2D(0,-200))
 @test willCollide(b, p, 1)
+end
+
+end
