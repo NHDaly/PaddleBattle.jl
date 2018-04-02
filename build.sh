93b6ef8a331efc23e7ccfd6168150fd2021b4fbe
Nathan Daly <nhdaly@gmail.com>
2018-04-02 00:21:59 -0400

    rename app file to "Paddle Battle.app"

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-APP_NAME="PaddleBattle"
+APP_NAME="Paddle Battle"
 jl_main="main"
 certificate="Nathan Daly"
 
 julia ~/src/build-jl-app-bundle/build_app.jl -v \
- -R assets -L "libs/*" --bundle_identifier "com.nhdalyMadeThis.paddlebattle" --icns icns.icns \
+ -R assets -L "libs/*" --bundle_identifier "com.nhdalyMadeThis.paddlebattle" --icns "icns.icns" \
  --certificate "$certificate" --entitlements "./entitlements.entitlements" \
  --app_version=0.2 "$jl_main.jl" "$APP_NAME"
