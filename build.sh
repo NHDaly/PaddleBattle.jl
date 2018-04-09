c98cde1164e8da2d667837826e1b01246ac944da
Nathan Daly <nhdaly@gmail.com>
2018-04-09 12:33:19 -0500

    add separate (commented out) App Store build rule

--- build.sh
+++ build.sh
@@ -2,9 +2,17 @@
 
 APPNAME="Paddle Battle"
 jl_main="main"
-certificate="Developer ID Application: nhdalyMadeThis, LLC"  # outside App Store
+dist_certificate="Developer ID Application: nhdalyMadeThis, LLC"  # outside App Store
+appstore_certificate="3rd Party Mac Developer Application: nhdalyMadeThis, LLC"
 
+# Build for distribution
 julia ~/src/build-jl-app-bundle/build_app.jl -v \
  -R assets -L "libs/*" --bundle_identifier "com.nhdalyMadeThis.paddlebattle" --icns "icns.icns" \
- --certificate "$certificate" --entitlements "./entitlements.entitlements" \
+ --certificate "$dist_certificate" --entitlements "./entitlements.entitlements" \
  --app_version=0.2 "$jl_main.jl" "$APPNAME"
+
+# # Build for macOS App Store
+# julia ~/src/build-jl-app-bundle/build_app.jl -v \
+#  -R assets -L "libs/*" --bundle_identifier "com.nhdalyMadeThis.paddlebattle" --icns "icns.icns" \
+#  --certificate "$appstore_certificate" --entitlements "./entitlements.entitlements" \
+#  --app_version=0.2 "$jl_main.jl" "$APPNAME" "./builddir/AppStore/"
