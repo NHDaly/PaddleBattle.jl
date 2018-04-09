20636a22f1609d107d573e179c2a420c1b8c9c9b
Nathan Daly <nhdaly@gmail.com>
2018-04-09 12:05:47 -0500

    Change app signing to nhdalyMadeThis LLC:
     - Changed the bundle identifier to com.nhdalyMadeThis.paddlebattle
     - Changed the signing certificate to my Apple Developer ID assigned
     certificate: "Developer ID Application: nhdalyMadeThis, LLC"
       - Note, this certificate is for signing apps that will be distributed
       outside the Mac App Store.

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-APP_NAME="Paddle Battle"
+APPNAME="Paddle Battle"
 jl_main="main"
-certificate="Nathan Daly"
+certificate="Developer ID Application: nhdalyMadeThis, LLC"  # outside App Store
 
 julia ~/src/build-jl-app-bundle/build_app.jl -v \
  -R assets -L "libs/*" --bundle_identifier "com.nhdalyMadeThis.paddlebattle" --icns "icns.icns" \
  --certificate "$certificate" --entitlements "./entitlements.entitlements" \
- --app_version=0.2 "$jl_main.jl" "$APP_NAME"
+ --app_version=0.2 "$jl_main.jl" "$APPNAME"
