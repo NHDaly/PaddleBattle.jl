e70d6504053eea10341d650a01752dd0dbf0ee4f
Nathan Daly <nhdaly@gmail.com>
2018-04-15 18:50:10 -0500

    removed Mac store build step

--- build.sh
+++ build.sh
@@ -3,16 +3,9 @@
 APPNAME="Paddle Battle"
 jl_main="main"
 dist_certificate="Developer ID Application: nhdalyMadeThis, LLC"  # outside App Store
-appstore_certificate="3rd Party Mac Developer Application: nhdalyMadeThis, LLC"
 
 # Build for distribution
 julia ~/src/build-jl-app-bundle/build_app.jl -v \
  -R assets -L "libs/*" --bundle_identifier "com.nhdalyMadeThis.Paddle-Battle" --icns "icns.icns" \
  --certificate "$dist_certificate" --entitlements "./entitlements.entitlements" \
- --app_version=0.2 "$jl_main.jl" "$APPNAME"
-
-# # Build for macOS App Store
-# julia ~/src/build-jl-app-bundle/build_app.jl -v \
-#  -R assets -L "libs/*" --bundle_identifier "com.nhdalyMadeThis.paddlebattle" --icns "icns.icns" \
-#  --certificate "$appstore_certificate" --entitlements "./entitlements.entitlements" \
-#  --app_version=0.2 "$jl_main.jl" "$APPNAME" "./builddir/AppStore/"
+ --app_version=1.0 "$jl_main.jl" "$APPNAME"
