fd3d7eaa023971978398412cbdc63665a4a70b8e
Nathan Daly <nhdaly@gmail.com>
2018-04-24 05:01:10 -0500

    Oops, put --certificate back on build.sh

--- build.sh
+++ build.sh
@@ -9,5 +9,5 @@ appstore_certificate="3rd Party Mac Developer Application: nhdalyMadeThis, LLC" 
 julia ~/src/build-jl-app-bundle/build_app.jl -v \
  -R assets -L "libs/*"  --icns "icns.icns" \
  --bundle_identifier "com.nhdalyMadeThis.Paddle-Battle" \
+ --certificate "$dist_certificate" --entitlements "./entitlements.entitlements" \
  --app_version=1.0.2 "$jl_main.jl" "$APPNAME"
-
