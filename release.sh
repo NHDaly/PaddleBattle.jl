f2c337971e3a6d1cb4ef47462c5dc6a6a148e5f2
Nathan Daly <nhdaly@gmail.com>
2018-05-05 17:13:05 -0500

    Add script to create+sign .dmg release to version control
    Added tiny, empty .dmg with placeholder .app for setting up the Finder
    settings for the output .dmg. The script will then copy .dmg and grow it
    in a tmp, then use that to build the final output.

---
+++ release.sh
@@ -0,0 +1,22 @@
+# Load the read/writeable dmg with manually set Finder settings.
+cp ./dmg/Paddle-Battle-rw.dmg ./builddir/Paddle-Battle-rw-tmp.dmg
+# Resize the .dmg to fit the app (plus a 50MB buffer for leeway):
+hdiutil resize -size "$(julia -E "M=1; $(du -hs builddir/Paddle\ Battle.app  | awk '{print $1}') + 50")m"  ./builddir/Paddle-Battle-rw-tmp.dmg
+hdiutil attach ./builddir/Paddle-Battle-rw-tmp.dmg
+
+# Replace the existing .app so that the position/size of the icon in Finder is preserved.
+stat /Volumes/Paddle\ Battle/Paddle\ Battle.app || exit 1
+rm -rf /Volumes/Paddle\ Battle/Paddle\ Battle.app    # Remove current contents of app (in case you've removed/renamed files)
+cp -r ./builddir/Paddle\ Battle.app  /Volumes/Paddle\ Battle/   # Replace .app with new contents
+
+hdiutil detach $(hdiutil info | grep '/Volumes/Paddle Battle' | awk '{print $1}')
+
+# "Save" the modified r/w .dmg to a read-only, compressed .dmg
+hdiutil convert -format UDZO -o ./builddir/Paddle-Battle-rw-converted.out.dmg  ./builddir/Paddle-Battle-rw-tmp.dmg
+mv ./builddir/Paddle-Battle-rw-converted.out.dmg ./builddir/Paddle-Battle.dmg
+
+# Sign it
+cp builddir/Paddle-Battle.dmg builddir/Paddle-Battle-unsigned.dmg
+codesign -s "Developer ID Application: nhdalyMadeThis, LLC" builddir/Paddle-Battle.dmg 
+
+
