de1ff0ba8b856bdb73236e24be515a11b59caa72
Nathan Daly <nhdaly@gmail.com>
2018-05-05 18:36:24 -0500

    Tag v1.0.3
    Fix codesign in .dmg release process.

--- build.sh
+++ build.sh
@@ -10,4 +10,4 @@ julia ~/src/build-jl-app-bundle/build_app.jl -v \
  -R assets -L "libs/*"  --icns "icns.icns" \
  --bundle_identifier "com.nhdalyMadeThis.Paddle-Battle" \
  --certificate "$dist_certificate" --entitlements "./entitlements.entitlements" \
- --app_version=1.0.2 "$jl_main.jl" "$APPNAME"
+ --app_version=1.0.3 "$jl_main.jl" "$APPNAME"
