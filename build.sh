7543e9f7f9a61998166891c8f52c0b2ec5975090
Nathan Daly <nhdaly@gmail.com>
2018-05-05 19:26:06 -0500

    Switch build.sh to ApplicationBuilder.jl instead of local clone

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ dist_certificate="Developer ID Application: nhdalyMadeThis, LLC"  # outside App 
 appstore_certificate="3rd Party Mac Developer Application: nhdalyMadeThis, LLC"  # outside App Store
 
 # Build for distribution
-julia ~/src/build-jl-app-bundle/build_app.jl -v \
+julia ~/.julia/v0.6/ApplicationBuilder/build_app.jl -v \
  -R assets -L "libs/*"  --icns "icns.icns" \
  --bundle_identifier "com.nhdalyMadeThis.Paddle-Battle" \
  --certificate "$dist_certificate" --entitlements "./entitlements.entitlements" \
