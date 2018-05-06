1aeb10d05281723cd15eaf95fafdc49a5d4a8abe
Nathan Daly <nhdaly@gmail.com>
2018-05-05 20:47:23 -0500

    Tag v1.0.4 to match Mac App Store version.

--- build.sh
+++ build.sh
@@ -10,4 +10,4 @@ julia ~/.julia/v0.6/ApplicationBuilder/build_app.jl -v \
  -R assets -L "libs/*"  --icns "icns.icns" \
  --bundle_identifier "com.nhdalyMadeThis.Paddle-Battle" \
  --certificate "$dist_certificate" --entitlements "./entitlements.entitlements" \
- --app_version=1.0.3 "$jl_main.jl" "$APPNAME"
+ --app_version=1.0.4 "$jl_main.jl" "$APPNAME"
